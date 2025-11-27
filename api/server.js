373ba8d8eeefd4c050b7fac4818b7daf881ab86d
aayushneupane <aayushneupane55@gmail.com>
2025-11-26 19:49:36 -0500

    CORS

--- api/server.js
+++ api/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // ====== CORS CONFIG ======
 app.use(cors({
-  origin: "https://www.loomia.fun",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
   credentials: true
